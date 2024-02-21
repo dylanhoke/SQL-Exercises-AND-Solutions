7861c7409c04974f0fff4b5c50e1fa287c55a374
dylanmhoke <dylan.m.hoke@outlook.com>
2024-02-21 03:20:38 -0800

    Similar Suspect names solution

---
+++ SuspectSimilarName.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE solution()
+BEGIN
+	SELECT id, name, surname
+	FROM Suspect
+	WHERE
+		height <= 170
+		AND SUBSTRING(name, 1, 1) = 'B'
+		AND surname LIKE 'Gre_n'
+	ORDER BY
+		id ASC;
+	
+END
